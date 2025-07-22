7c0baa2869dc863a9d5d805cb98de9a9e6577762
Coby Scrudder <cobyscrudder@tamu.edu>
2025-07-22 14:26:13 -0400

    Update UHS_sintering_script.py

--- UHS_sintering_script.py
+++ UHS_sintering_script.py
@@ -82,6 +82,9 @@ def plot_voltage(n):
 
 
 # Initialize session state for voltage steps
+if "repeat_times" not in st.session_state:
+    st.session_state.repeat_times = "0"  # or a sensible default like 1
+
 if "steps" not in st.session_state:
     st.session_state.steps = []
 
